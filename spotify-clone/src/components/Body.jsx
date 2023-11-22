89716f42e2a9852f07c2e32222c7042c4a5f4fe6
unknown <shivamswami8899@gmail.com>
2023-11-22 23:15:40 +0530

    selectplaylist done

--- spotify-clone/src/components/Body.jsx
+++ spotify-clone/src/components/Body.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { AiFillClockCircle } from "react-icons/ai";
+import { useStateProvider } from "../utils/stateProvider";
+import axios from "axios";
+import { reducerCases } from "../utils/Constants";
 
 export default function Body() {
-  return <div>Body</div>;
+  const [{ token, selectedPlayListId, selectedPlayList }, dispatch] =
+    useStateProvider();
+  useEffect(() => {
+    const getInitialPlayList = async () => {
+      const res = await axios.get(
+        `https://api.spotify.com/v1/playlists/${selectedPlayListId}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const selectedPlayList = {
+        id: res.data.id,
+        name: res.data.name,
+        description: res.data.description.startsWith("<a")
+          ? ""
+          : res.data.description,
+        image: res.data.images[0].url,
+        tracks: res.data.tracks.items.map(({ track }) => ({
+          id: track.id,
+          name: track.name,
+          artists: track.artists.map((artist) => artist.name),
+          image: track.album.images[2].url,
+          duration: track.duration_ms,
+          album: track.album.name,
+          context_uri: track.album.uri,
+          track_number: track.track_number,
+        })),
+      };
+      dispatch({ type: reducerCases.SET_PLAYLIST, selectedPlayList });
+    };
+    getInitialPlayList();
+  }, [token, dispatch, selectedPlayListId]);
+  return (
+    <Container>
+      {selectedPlayList && (
+        <>
+          <div className="playlist">
+            <div className="image">
+              <img src={selectedPlayList.image} alt="" />
+            </div>
+          </div>
+        </>
+      )}
+    </Container>
+  );
 }
+
+const Container = styled.div``;
--- spotify-clone/src/utils/reducer.js
+++ spotify-clone/src/utils/reducer.js
@@ -4,6 +4,8 @@ export const initialState = {
   token: null,
   playlists: [],
   userInfo: null,
+  selectedPlayListId: "6JuUJHBLrt3iKGYkoQ2QI9",
+  selectedPlayList: null,
 };
 
 const reducer = (state, action) => {
@@ -24,6 +26,12 @@ const reducer = (state, action) => {
       return {
         ...state,
         userInfo: action.userInfo,
+      };
+    }
+    case reducerCases.SET_PLAYLIST: {
+      return {
+        ...state,
+        selectedPlayList: action.selectedPlayList,
       };
     }
     default:
