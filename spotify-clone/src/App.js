865ddca367d47d3d5f7715ed8c8cbe5c358cf183
unknown <shivamswami8899@gmail.com>
2023-11-22 16:25:59 +0530

    login part done

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,5 +1,20 @@
+import Login from "./components/Login";
+import React, { useEffect } from "react";
+import { useStateProvider } from "./utils/stateProvider";
+import { reducerCases } from "./utils/Constants";
+import Spotify from "./components/Spotify";
+
 function App() {
-  return <div className="App">App</div>;
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+
+  return <div className="App">{token ? <Spotify /> : <Login />}</div>;
 }
 
 export default App;
